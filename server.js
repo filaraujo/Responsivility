4ddd90d6356b759c23802031abe939c2cde27a7c
Filipe Araujo <filipe.alves.araujo@gmail.com>
2012-02-18 17:16:22 -0500

    adding express

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
-var http = require('http');
 
-var server = http.createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/plain" })
-    res.end("Hello world\n");
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+    , routes = require('./routes');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
 });
 
-server.listen(process.env.PORT || 8001);
+app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+    app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', routes.index);
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
